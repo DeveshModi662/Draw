d49cd599cfff7b8dcfd16eb7e7390adb35590c8a
DeveshModi662 <deveshmodi662@gmail.com>
2025-10-04 21:23:40 +0530

    simple single page sso working

--- doroui/src/App.js
+++ doroui/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import AuthPage from './AuthPage';
+import Draw from './Draw';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState() ;
+
+  useEffect(() => {
+    console.log('App - useEffect') ;
+    console.log(localStorage.getItem("user")) ;
+    if(localStorage.getItem("user") && localStorage.getItem("jsonWebToken")) { 
+      console.log('App useEffect : ', localStorage.getItem("user"), localStorage.getItem("jsonWebToken")) ;
+      fetch(`http://localhost:8080/isLoggedIn/`, 
+        {
+          method : 'GET',
+          headers : {
+            'Authorization': `Bearer ${localStorage.getItem("jsonWebToken")}`
+          }
+        }
+      )
+      .then(response => {
+          if(!response.ok) {
+            console.log('App - useEffect - Not logged in') ;
+            console.log(response) ;
+            handleLogout() ;
+          }
+          else {
+            setUser(localStorage.getItem("user")) ;
+          }
+          
+        }
+      ) ;      
+    }
+  }, []) ;
+
+  const handleAuth = (loggedinUser) => {
+    setUser(loggedinUser.user) ;
+    localStorage.setItem("user", loggedinUser.user)
+    localStorage.setItem("jsonWebToken", loggedinUser.jsonWebToken)
+  } ;
+
+  const handleLogout = () => {
+    localStorage.removeItem("user") ;
+    localStorage.removeItem("jsonWebToken") ;
+    setUser() ;
+  } ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>handleLogout()}>LOGOUT</button>
+      {
+        user ? (<Draw />)
+        : (<AuthPage onAuth={handleAuth} />) 
+      }
     </div>
   );
 }
