03a0e27d420606b9b045323e1cac43e24678ca04
DeveshModi662 <deveshmodi662@gmail.com>
2025-10-04 21:00:42 +0530

    line rect working

--- doroui/src/Draw.js
+++ doroui/src/Draw.js
@@ -1,4 +1,4 @@
-import React, {useLayoutEffect, useState, useRef, useCallback, useEffect} from "react" ;
+import React, {useState, useRef, useEffect} from "react" ;
 import rough from 'roughjs' ;
 
 const generator = rough.generator() ;
