a30310f5e1b11d5806408654e3b8ff6c5554c940
DeveshModi662 <deveshmodi662@gmail.com>
2025-11-19 20:19:13 +0530

    export wo auth working from UI

--- exportservice/src/main/java/com/exportservice/exportservice/controller/ExportController.java
+++ exportservice/src/main/java/com/exportservice/exportservice/controller/ExportController.java
@@ -3,6 +3,7 @@ package com.exportservice.exportservice.controller;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ public class ExportController {
     @Autowired
     private ExportService exportService ;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/{loggedInUsername}/canvas/{canvasId}")
     public ResponseEntity<byte[]> printCanvas(@PathVariable String loggedInUsername, @PathVariable String canvasId) {        
         return exportService.printCanvas(loggedInUsername, canvasId) ;
